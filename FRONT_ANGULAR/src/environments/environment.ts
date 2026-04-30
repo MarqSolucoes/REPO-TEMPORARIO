20e69b5ef61c1116f87157be8ffeb056f123961d
kaiov <kaiov@DESKTOP-3HJH0UK>
2026-04-29 21:42:42 -0300

    correcao erro jquery console

--- FRONT_ANGULAR/src/environments/environment.ts
+++ FRONT_ANGULAR/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //baseURLApi: 'http://whengenharia.solinski.com.br:5500/api'
-  baseURLApi: 'http://localhost:5500/api'
+  baseURLApi: 'http://whengenharia.solinski.com.br:5500/api'
+  //baseURLApi: 'http://localhost:5500/api'
 };
